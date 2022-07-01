182ae48c412cc81dba2dc116bc4f4cdaf720b267
FelixvL <digitaleservice@gmail.com>
2022-07-01 15:28:04 +0200

    CB

--- Demo.java
+++ Demo.java
@@ -1,7 +1,17 @@
-// CA
+// CB
 
 class Demo{
-	public static void start(String[] args){
+	public static void main(String[] args){
 		System.out.println("start");
+		Fiets f = new Fiets();
+		f.main(null);
+	}
+}
+
+
+class Fiets{
+
+	public void main(String[] args){
+		throw new RuntimeException();
 	}
 }
