f9d0e272eca4576bdc4cf409334339d469684a27
FelixvL <digitaleservice@gmail.com>
2022-07-01 15:34:33 +0200

    CD

--- Demo.java
+++ Demo.java
@@ -1,17 +1,24 @@
-// CC
+// CD
 
 class Demo{
 	public static void main(String[] args){
 		System.out.println("start");
-		Fiets f = new Fiets();
-		f.main(null);
+		MountainBike f = new MountainBike();
+		f.rondFietsen();
 	}
 }
 
 
 class Fiets{
 
-	public void main(String[] args){
+	public void rondFietsen()throws Exception{
+		System.out.println("in fiets");
 		throw new Exception();
+	}
+}
+
+class MountainBike{
+	void rondFietsen(){
+		System.out.println("in mtb");
 	}
 }
