935c051e1cfed3506648586a949285c27774eb0a
FelixvL <digitaleservice@gmail.com>
2022-07-01 15:24:10 +0200

    CA

--- Demo.java
+++ Demo.java
@@ -1,31 +1,7 @@
-// BO
+// CA
 
 class Demo{
-	public static void main(String [] args){
-		System.out.println("hoi");
-		Wandelaar joost = new SnelWandelaar();
-		joost.wandelen(5);
+	public static void start(String[] args){
+		System.out.println("start");
 	}
 }
-class Wandelaar{
-	int a;
-	Wandelaar(int z){
-		System.out.println("ik ben er");
-	}
-	Wandelaar wandelen(int a){
-		System.out.println(a++);
-		return new Wandelaar();
-	}
-}
-class SnelWandelaar extends Wandelaar{
-	int a;
-	Wandelaar wandelen(int b){
-		System.out.println("dat gaat snel");
-		return null;
-	}
-
-}
-//1 zelfde sig
-//2 returntype covariant
-//3 access modifier NIET kleiner
-//4 throws covariant
