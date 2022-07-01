32be0f75835426f716c09808bf9f93a63f94f0df
FelixvL <digitaleservice@gmail.com>
2022-07-01 10:58:45 +0200

    BD

--- Demo.java
+++ Demo.java
@@ -1,9 +1,13 @@
-// BC
+// BD
 
 class Demo{
 	public static void main(String [] args){
 		System.out.println("hoi");
 		int a = 5;
 		System.out.println(a++);
+		gaan();
+	}
+	void gaan(){
+		System.out.println("iets");
 	}
 }
