f00761503a7e306fefd184c59bf3872e144998e3
FelixvL <digitaleservice@gmail.com>
2022-07-01 11:36:52 +0200

    BK

--- Demo.java
+++ Demo.java
@@ -1,4 +1,4 @@
-// BJ
+// BK
 
 class Demo{
 	public static void main(String [] args){
@@ -6,11 +6,15 @@ class Demo{
 		Wandelaar joost = new Wandelaar();
 		Wandelaar frits = new Wandelaar();
 		frits.a++;
+		frits.wandelen(85);
 		System.out.println(frits.wandelen(4).a);
 	}
 }
 class Wandelaar{
-	static int a;
+	int a;
+	Wandelaar(){
+		System.out.println("ik ben er");
+	}
 	Wandelaar wandelen(int a){
 		System.out.println(a++);
 		return new Wandelaar();
