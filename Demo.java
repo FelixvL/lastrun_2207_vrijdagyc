51496ece24689fb14eddf68459073762b0fa468f
FelixvL <digitaleservice@gmail.com>
2022-07-01 11:02:59 +0200

    BE

--- Demo.java
+++ Demo.java
@@ -1,13 +1,21 @@
-// BD
+// BE
 
 class Demo{
 	public static void main(String [] args){
 		System.out.println("hoi");
 		int a = 5;
 		System.out.println(a++);
-		gaan();
+		new Demo.gaan();
+		System.out.println(a);
 	}
 	void gaan(){
 		System.out.println("iets");
+		new Wandelaar().wandelen();
+	}
+}
+class Wandelaar{
+	double a;
+	void wandelen(){
+		System.out.println(a);
 	}
 }
